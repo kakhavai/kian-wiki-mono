16ba53c870cd20b7639c6552390a8d801505da09
kakhavai <kakhavain55@gmail.com>
2024-05-18 01:14:08 -0700

    Added IWrMatchStatsDTO (#14)
    Co-authored-by: kakhavai <kakhavain@DESKTOP-SOB3D4E>

---
+++ packages/nfl-feed-scraper/src/types/dto/IWrMatchStatsDTO.ts
@@ -0,0 +1,120 @@
+export interface IWrMatchStats {
+  playerID: string;
+  gameID: string;
+  away: string;
+  home: string;
+  fantasyPoints: number;
+  fumblesLost: number;
+  passAttempts: number;
+  passTD: number;
+  passYds: number;
+  int: number;
+  passingTwoPointConversion: number;
+  passCompletions: number;
+  rushAvg: number;
+  rushYds: number;
+  carries: number;
+  longRush: number;
+  rushTD: number;
+  receptions: number;
+  recTD: number;
+  longRec: number;
+  targets: number;
+  recYds: number;
+  recAvg: number;
+  receivingTwoPointConversion: number;
+  //   fantasyPointsDefault: {
+  //     standard: number;
+  //     PPR: number;
+  //     halfPPR: number;
+  //   };
+
+  // Scoring plays (if applicable)
+  //   scoringPlays?: Array<{
+  //     score: string;
+  //     scorePeriod: string;
+  //     homeScore: string;
+  //     awayScore: string;
+  //     teamID: string;
+  //     scoreDetails: string;
+  //     scoreType: string;
+  //     scoreTime: string;
+  //     team: string;
+  //     playerIDs: string[];
+  //   }>;
+
+  // Defense statistics (if applicable)
+  //   Defense?: {
+  //     totalTackles: number;
+  //     defTD: number;
+  //     soloTackles: number;
+  //     tfl: number;
+  //     qbHits: number;
+  //     defensiveInterceptions: number;
+  //     sacks: number;
+  //     passDeflections: number;
+  //     fumblesLost?: number;
+  //     fumbles?: number;
+  //     fumblesRecovered?: number;
+  //   };
+
+  // Rushing statistics (if applicable)
+  //   Rushing?: {
+  //     rushAvg: number;
+  //     rushYds: number;
+  //     carries: number;
+  //     longRush: number;
+  //     rushTD: number;
+  //   };
+
+  // Passing statistics (if applicable)
+  //   Passing?: {
+  //     qbr: number;
+  //     rtg: number;
+  //     sacked: string;
+  //     passAttempts: number;
+  //     passAvg: number;
+  //     passTD: number;
+  //     passYds: number;
+  //     int: number;
+  //     passingTwoPointConversion?: number;
+  //     passCompletions: number;
+  //   };
+
+  //   // Receiving statistics (if applicable)
+  //   Receiving?: {
+  //     receptions: number;
+  //     recTD: number;
+  //     longRec: number;
+  //     targets: number;
+  //     recYds: number;
+  //     recAvg: number;
+  //     receivingTwoPointConversion?: number;
+  //   };
+
+  // Kicking statistics (if applicable)
+  //   Kicking?: {
+  //     fgLong: number;
+  //     fgMade: number;
+  //     fgAttempts: number;
+  //     xpMade: number;
+  //     fgPct: number;
+  //     kickingPts: number;
+  //     xpAttempts: number;
+  //   };
+
+  // Punting statistics (if applicable)
+  //   Punting?: {
+  //     puntYds: number;
+  //     punts: number;
+  //     puntAvg: number;
+  //     puntsin20: number;
+  //     puntTouchBacks: number;
+  //     puntLong: number;
+  //     puntReturns?: number;
+  //     puntReturnYds?: number;
+  //     puntReturnAvg?: number;
+  //     puntReturnLong?: number;
+  //     puntReturnTD?: number;
+  //   };
+}
