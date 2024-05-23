ffc1f00386ebcfef9eadb77fdb3f8e80b0026d80
kakhavai <kakhavain55@gmail.com>
2024-05-22 18:31:31 -0700

    Initial UI commit (#19)
    Co-authored-by: kakhavai <kakhavain@DESKTOP-SOB3D4E>

---
+++ packages/kian-wiki-frontend/.eslintrc.js
@@ -0,0 +1,7 @@
+module.exports = {
+  extends: [
+    'next/core-web-vitals',
+    '../../.eslintrc.js', // Path to your main ESLint configuration file
+  ],
+  // Additional rules or overrides for this specific directory can go here
+};
