83882b2eb21d0e592f1b718dad8869395d83b303
kakhavai <kakhavain@DESKTOP-SOB3D4E>
2024-05-26 01:02:01 -0700

    moved to next/font

--- packages/kian-wiki-frontend/src/app/layout.tsx
+++ packages/kian-wiki-frontend/src/app/layout.tsx
@@ -1,7 +1,7 @@
 import type { Metadata } from 'next';
 import { Inter } from 'next/font/google';
 import './globals.css';
-import { NextFont } from '@next/font';
+import { NextFont } from "next/font";
 
 const inter: NextFont = Inter({ subsets: ['latin'] });
 
