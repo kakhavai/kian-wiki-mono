9817d38ed4f349de0aa2dc1c9ebc47a5dfa281e8
kakhavai <kakhavain55@gmail.com>
2024-06-09 00:01:43 -0700

    automated serverless deployment hello-world (#27)
    Co-authored-by: kakhavai <kakhavain@DESKTOP-SOB3D4E>

--- packages/lambda-api-gateway/src/handler.ts
+++ packages/lambda-api-gateway/src/handler.ts
@@ -1,14 +1,14 @@
 import { APIGatewayEvent, APIGatewayProxyResult } from 'aws-lambda';
-import { TeamDataService } from 'nfl-feed-scraper';
+// import { TeamDataService } from 'nfl-feed-scraper';
 
-const teamDataService: TeamDataService = new TeamDataService();
+// const teamDataService: TeamDataService = new TeamDataService();
 
 export const mainHandler = async (
   event: APIGatewayEvent,
 ): Promise<APIGatewayProxyResult> => {
   console.log('Event:', event);
 
-  await teamDataService.updateTeamRecords();
+  // await teamDataService.updateTeamRecords();
   return {
     statusCode: 200,
     body: JSON.stringify({ message: 'Hello from Lambda API Gateway!' }),
